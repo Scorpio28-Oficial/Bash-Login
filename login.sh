cebcf627ba8b5654704fd047f6999199627ff915
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-07-11 00:55:24 -0500

    Update login.sh

--- login.sh
+++ login.sh
@@ -21,7 +21,7 @@ echo -e "
 \e[1;32m█▄▄█ ▀░░▀ ▀▀▀ ▀░░▀ \e[1;30m█▄▄█ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀░░▀
 \e[1;32m
 [+] Creator: Scorpio28
-[+] Team: Darkmux Conmunity
+[+] Team: Informatic in Termux
 [+] Github: https://github.com/Scorpio28-Oficial
 \e[0m"
 read -p $'\e[1;30m
@@ -52,7 +52,7 @@ echo -e "
 \e[1;30m║\e[1;32m  IIIIIII. I  II  .    II..IIIIIIIIIIIIIIIIIIII       \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m .  .IIIIII  II          III \e[1;37mScorpio28\e[1;32m IIIIIIIIII.    \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m    .IIIII.III I      IIIIIIIII \e[1;37mThe\e[1;32m IIIIIIIII  I.     \e[1;30m║\e[1;32m
-\e[1;30m║\e[1;32m   .IIIIII \e[1;37mHacking\e[1;32m II  .IIII \e[1;37mMexicano\e[1;32m IIII. III       \e[1;30m║\e[1;32m
+\e[1;30m║\e[1;32m   .IIIIII \e[1;37mHacking\e[1;32m II  .IIII \e[1;37mMexican\e[1;32m IIIII. III       \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m    IIIIIII \e[1;37mFrom\e[1;32m    ' IIIII I IIIIIIIIIIII III I      \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m    .II    \e[1;37mTermux\e[1;32m      IIIIIIIIIIII  IIIIIIIIII       \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m       I.           .IIIIIIIIIIII   I   II  I         \e[1;30m║\e[1;32m
@@ -63,7 +63,7 @@ echo -e "
 \e[1;30m║\e[1;32m            IIII           III. I            IIIEIII  \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m            III             I                I  IPI   \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m             II       \e[1;30m[\e[1;31m+\e[1;30m] \e[1;37mHacking \e[1;30m[\e[1;31m+\e[1;30m]\e[1;32m        D   .    \e[1;30m║\e[1;32m
-\e[1;30m║\e[1;32m             I             \e[1;33mÉtico\e[1;32m                      \e[1;30m║\e[1;32m
+\e[1;30m║\e[1;32m             I            \e[1;33mEthical\e[1;32m                     \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m                                                      \e[1;30m║\e[1;32m
 \e[1;30m║\e[1;32m \e[1;30m[\e[1;36m>\e[1;30m] \e[1;30m[\e[1;36m>\e[1;30m] \e[1;30m[\e[1;36m>\e[1;30m]\e[1;37m $(date) \e[1;30m[\e[1;36m<\e[1;30m] \e[1;30m[\e[1;36m<\e[1;30m] \e[1;30m[\e[1;36m<\e[1;30m] \e[1;30m║\e[1;32m
 \e[1;30m╚══════════════════════════════════════════════════════╝
